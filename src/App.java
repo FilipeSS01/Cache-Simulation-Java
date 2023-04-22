627118881e3a733e4ead1aa14c322cdd2f8410ec
Filipe <filipess671@gmail.com>
2023-04-21 22:09:14 -0300

    Cache: Adicionando mapeamento associativo

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
+import mappings.Associative;
 import mappings.Direct;
-
 
 public class App {
     public static void main(String[] args) {
         String path = App.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         new Direct(path);
+        new Associative(path);
     }
 
 }
---
+++ src/mappings/Associative.java
@@ -0,0 +1,75 @@
+package mappings;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import abstracts.Mappings;
+import utils.Convert;
+import utils.FileManager;
+
+public class Associative extends Mappings {
+
+    private Map<Integer, String> cache;
+
+    public Associative(String path) {
+        super(path);
+    }
+
+    @Override
+    protected void initialize() {
+        // Read config
+        ArrayList<String> dadosConfig = FileManager.stringReader(getPath() + "data/config/config.txt");
+        String[] memoryConfig = dadosConfig.get(0).split("[ #@_\\/.*;]");
+        String[] wordConfig = dadosConfig.get(1).split("[ #@_\\/.*;]");
+        // String[] cacheConfig = dadosConfig.get(2).split("[ #@_\\/.*;]");
+        // String[] lineConfig = dadosConfig.get(3).split("[ #@_\\/.*;]");
+
+        // Value in Bytes
+        Integer memoryBytes = convertToBits(Integer.parseInt(memoryConfig[2]), memoryConfig[3]);
+        Integer wordBytes = convertToBits(Integer.parseInt(wordConfig[2]), wordConfig[3]);
+        // Integer cacheBytes = convertToBits(Integer.parseInt(cacheConfig[2]),
+        // cacheConfig[3]);
+
+        setCache(new HashMap<Integer, String>());
+
+        // Values in bits
+        setAddressBits(calcAddress(memoryBytes, wordBytes));
+        setWordBits(calcWord(wordBytes));
+        setLineBits(0);
+        setTagBits(calcTag(getAddressBits(), getWordBits(), getLineBits()));
+
+        // Read memory
+        readMemory();
+    }
+
+    @Override
+    protected void mapping(String[] partAddress) {
+        Integer tag = Integer.parseInt(partAddress[0], 2);
+        if (!getCache().containsKey(tag)) {
+            getCache().put(tag, partAddress[1]);
+            setErrors(getErrors() + 1);
+        } else {
+            setHits(getHits() + 1);
+        }
+    }
+
+    @Override
+    protected String[] getPartAddress(Integer value) {
+        String[] partAddress = new String[2];
+        String addressBinary = Convert.intToBinaryString(value, getAddressBits());
+        partAddress[0] = addressBinary.substring(0, getTagBits()); // Tag
+        partAddress[1] = addressBinary.substring(getTagBits(), getAddressBits()); // Word
+        return partAddress;
+    }
+
+    // Gets and Sets
+    public Map<Integer, String> getCache() {
+        return cache;
+    }
+
+    private void setCache(Map<Integer, String> cache) {
+        this.cache = cache;
+    }
+
+}
