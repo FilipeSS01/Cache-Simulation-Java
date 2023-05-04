600090f738588fcbf08a599e46690befc3e45d8a
Filipe <filipess671@gmail.com>
2023-05-04 20:00:25 -0300

    Cache: App.java

--- src/App.java
+++ src/App.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+
 import mappings.Associative;
 import mappings.Direct;
+import utils.FileManager;
 
 public class App {
     public static void main(String[] args) {
         String path = App.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        new Direct(path);
-        new Associative(path, 2);
+        ArrayList<String> memoryData = FileManager.stringReader(path + "data/others/memory3.txt");
+        ArrayList<String> dataConfig = FileManager.stringReader(path + "data/config/config.txt");
+
+        new Direct(memoryData, dataConfig);
+        new Associative(memoryData, dataConfig, 1);
+        // new Associative(memoryData, dataConfig, 2);
+        // new Associative(memoryData, dataConfig, 3);
+        // new Associative(memoryData, dataConfig, 4);
     }
 
 }
