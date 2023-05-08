b96c56aa1ac6a10f01adc390e63f56089a0dda3b
Filipe <filipess671@gmail.com>
2023-05-08 13:07:03 -0300

    Cache: Otimizando código de substituição

--- src/replacement/Replacement.java
+++ src/replacement/Replacement.java
@@ -1,27 +1,23 @@
 package replacement;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 public class Replacement {
+    private static final Random random = new Random();
+    private static final Comparator<Map.Entry<Integer, Integer>> LFU_COMPARATOR = Comparator
+            .comparing(Map.Entry::getValue);
+
     public static void random(Map<Integer, String> cache) {
-        Random random = new Random();
-        Integer[] tagList = cache.keySet().toArray(new Integer[cache.size()]);
-        Integer randomTag = tagList[random.nextInt(tagList.length)];
+        Integer randomTag = cache.keySet().iterator().next();
         cache.remove(randomTag);
     }
 
     public static void random(ArrayList<Integer> arrayList) {
-        Random random = new Random();
         arrayList.remove(random.nextInt(arrayList.size()));
     }
 
     public static void fifo(Map<Integer, String> cache) {
-        Integer[] tagList = cache.keySet().toArray(new Integer[cache.size()]);
-        Integer firstTag = tagList[0];
+        Integer firstTag = cache.keySet().iterator().next();
         cache.remove(firstTag);
     }
 
@@ -30,20 +26,17 @@ public class Replacement {
     }
 
     public static void lfu(Map<Integer, String> cache, Map<Integer, Integer> auxCache) {
-        Map.Entry<Integer, Integer> min = Collections.min(auxCache.entrySet(),
-                new Comparator<Map.Entry<Integer, Integer>>() {
-                    public int compare(Map.Entry<Integer, Integer> entry1, Map.Entry<Integer, Integer> entry2) {
-                        return entry1.getValue().compareTo(entry2.getValue());
-                    }
-                });
-
+        PriorityQueue<Map.Entry<Integer, Integer>> priorityQueue = new PriorityQueue<>(LFU_COMPARATOR);
+        priorityQueue.addAll(auxCache.entrySet());
+        Map.Entry<Integer, Integer> min = priorityQueue.poll();
         cache.remove(min.getKey());
         auxCache.remove(min.getKey());
     }
 
     public static void lru(Map<Integer, String> cache, ArrayList<Integer> listAuxCache) {
-        Integer firstTag = listAuxCache.get(0);
-        listAuxCache.remove(0);
+        Iterator<Integer> iterator = listAuxCache.iterator();
+        Integer firstTag = iterator.next();
+        iterator.remove();
         cache.remove(firstTag);
     }
 
@@ -53,9 +46,8 @@ public class Replacement {
             Integer aux = arrayList.get(index);
             arrayList.remove(index);
             arrayList.add(aux);
-        }else{
+        } else {
             arrayList.remove(0);
         }
     }
-
 }
